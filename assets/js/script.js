986fdc36f37a71dcdb2c29697fce6cbf326e2271
suriya babaei <suriyalifework60@gmail.com>
2023-07-10 08:13:09 -0700

    create move.html and style mpt and script

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,50 @@ const swiper = new Swiper(".card-slider", {
   },
 });
 
+// video player
+
+const video = document.querySelector(".video");
+const toggleButton = document.querySelector(".toggleButton");
+const progress = document.querySelector(".progressvideo");
+const progressBar = document.querySelector(".progress_filled");
+function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+function updateToggleButton() {
+  toggleButton.innerHTML = video.paused ? "►" : "❚❚";
+}
+// progrees
+function handleProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+// scrub
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+toggleButton.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateToggleButton);
+video.addEventListener("pause", updateToggleButton);
+// progrees
+video.addEventListener("timeupdate", handleProgress);
+// scrub
+
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mouseup", () => (mousedown = false));
+
+
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") togglePlay();
+});
